ee3c869fafdadc07c1caeb98cbc7a0a87f16b80c
bilmasam <asadbeksadirov70@gmail.com>
2023-08-16 19:11:43 +0500

    second commit

--- home_Page/js/app.js
+++ home_Page/js/app.js
@@ -1,13 +1,96 @@
 'use strict'
 
-$(document).ready(function(event) {
-    var top_header = $(".carousel .fill");
+//////////////////// 
+easy
+////////////////////
+
+
+function salomlashish() {
+  console.log("Salom Dunyo");
+}
+
+salomlashish()
+
+
+/////////////////////////////////////////////////////
+
+
+function user(name) {
+  name = "Kamil"
+  console.log(name);
+}
+
+
+user()
+
+
+/////////////////////////////////////////////////////
+
+function userQuestion() {
   
-    $(window).scroll(function() {
-      var st = $(this).scrollTop();
-      top_header.css({
-        "background-position": "center calc(50% + " + st * 0.8 + "px)"
-      });
-    });
-  });
-  
+  const askName = prompt("Ismingiz nima?","jhon");
+  const askAge = +prompt("yoshingizni kiriting", "16");
+  const askAddress = prompt("Manzilingizni kiriting", "navoi")
+  console.log(askName);
+  console.log(askAge);
+  console.log(askAddress);
+}
+userQuestion()
+
+
+///////////////////
+Callback Functions
+///////////////////
+
+function Salomlashish(text) {
+  text = "qalay"
+  console.log("Salom");
+}
+Salomlashish()
+
+
+/////////////////////////////////////////////////////
+
+function callback1(name, callback) {
+  name = "Tommy" 
+  console.log(`salom ${name}`);
+
+  function Callback() {
+    console.log("Callback function ishladi");
+  }
+
+  Callback( )
+}
+
+callback1()
+
+
+
+/////////////////////////////////////////////////////
+
+
+function greet(params) {
+  setTimeout(()=>{
+    console.log("Salom Dunyo ");
+} ,2000)
+}
+
+function sayName(name) {
+  name = "Jacob"
+  console.log(name);
+}
+
+greet();
+sayName();  
+
+
+///////////////////////////////////////////////////////
+
+
+function helloWorld(name, myFunction) {
+  myFunction
+  console.log("Hello World");
+}
+helloWorld()
+
+// 
